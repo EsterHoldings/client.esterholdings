a898a962f2499cfff890ea8a76d54466c83241ef
rodionmaksimenko <asvrodya@gmail.com>
2025-12-25 01:58:55 +0200

    Updates

---
+++ pages/admin/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { definePageMeta } from "~/.nuxt/imports";
+
+definePageMeta({
+  middleware: ["admin-root"],
+});
+</script>
